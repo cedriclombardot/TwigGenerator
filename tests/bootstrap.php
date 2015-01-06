4542435b32daec10ab79f7a39065fab16ad10780
William Durand <will+git@drnd.me>
2015-01-06 23:00:22 +0100

    fix test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,8 @@
 <?php
 
-if ((!$loader = @include __DIR__.'/../vendor/.composer/autoload.php')) {
-    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
-        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
-        'php composer.phar install'.PHP_EOL);
+if ((!$loader = @include __DIR__.'/../vendor/autoload.php')) {
+    die('You must set up the project dependencies, run the following command:'.PHP_EOL.PHP_EOL.
+        '    php composer.phar install'.PHP_EOL);
 }
 
 $loader->add('TwigGenerator\\Tests', 'tests');
